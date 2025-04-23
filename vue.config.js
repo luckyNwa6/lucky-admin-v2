435e4c9f1a082c15d2318193ae129e4e849aa507
luckyNwa <1656213092@qq.com>
2025-04-23 14:55:51 +0800

    fix：切换生产环境代理

--- vue.config.js
+++ vue.config.js
@@ -36,7 +36,7 @@ module.exports = {
     proxy: {
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
-        target: `https://localhost:10086/luckyAdmin`,
+        target: `http://localhost:10086/luckyAdmin`,
         changeOrigin: true,
         pathRewrite: {
           ['^' + process.env.VUE_APP_BASE_API]: '',
